f38a4640bce52334462e109986f5e1b68e5e2e9e
Len Kutchma <lkutchma@gmail.com>
2014-10-29 13:30:47 -0500

    v1.0.0
    Initial commit

---
+++ README.txt
@@ -0,0 +1,99 @@
+RAVEN CHILD THEME
+
+INSTALLATION
+
+	1. Upload the Raven child theme folder via FTP to your wp-content/themes/ directory. (The Genesis parent theme needs to be in the wp-content/themes/ directory as well).
+	2. Go to your WordPress dashboard and select Appearance.
+	3. Activate the Raven child theme.
+	4. Inside your WordPress dashboard, go to Genesis > Theme Settings and configure them to your liking.
+
+Note: After you download and unarchive the theme from GitHub, you may want to rename the raven-master folder to raven. (optional)
+
+WIDGET AREAS
+
+	Raven comes with 3 widget areas:
+
+	1. Welcome Text. Appears on the front page above the posts area. Use a text widget.
+	2. Before Entry. Appears before a post on single post pages. Use a text widget.
+	3. After Entry. Appears after a post on single post pages. Use a text widget or Genesis eNews Extended widget.
+
+IMAGES SIZES
+
+	Raven uses the following image sizes:
+
+	1. mini-square (80x80)
+	2. square (100x100)
+	3. large-square (120x120)
+
+PRIMARY NAVIGATION EXTRAS
+	As of Genesis 2.1, the Primary Navigation Extras options have been removed for new installs. (upgrades of current installs to 2.1 will not lose the Primary Navigation Extras settings)
+	The prefered method of adding custom links like Twitter or RSS is to use a custom menu item and give it a CSS class of right.
+
+	Raven includes support for the Genericons icon font for use with the Primary Navigation Extras. The list of supported icon fonts are:
+
+	- codepen
+	- dribble
+	- email
+	- facebook
+	- flickr
+	- foursquare
+	- github
+	- googleplus
+	- instagram
+	- linkedin
+	- pinterest
+	- reddit
+	- rss
+	- stumbleupon
+	- tumblr
+	- twitter
+	- vimeo
+	- wordpress
+	- youtube
+
+	For example, to add a Twitter link, add a custom link to your menu and in the CSS Classes input field enter right twitter. Refer to the theme docs for details.
+
+CONTENT BOXES
+	The custom content boxes make use of DIV classes. For example ...
+
+	<div class="content-box-blue">some random text</div>
+
+	There are 8 styles to choose from. Refer to the style sheet for details.
+
+GRADIENT BUTTONS
+	Each button has a unique class. For example ...
+
+	<a class="button-green" href="#">Link Text</a> 
+
+	The above will render the Green button. There are 11 styles to choose from. Refer to the style sheet for details.
+
+HEADER AREA IMAGE
+	Raven uses a logo in the header area which you will find in the theme's image folder. Create a new image with the same dimensions. To display it navigate to
+	Genesis > Theme Settings and select "Image logo" from the drop down menu.
+
+RECOMMENDED PLUGINS
+	1. Genesis eNews Extended - http://wordpress.org/extend/plugins/genesis-enews-extended/
+	2. Genesis Widgetized Archive - http://wordpress.org/plugins/genesis-widgetized-archive/
+	3. Genesis Widgetized Not Found & 404 - http://wordpress.org/plugins/genesis-widgetized-notfound/
+	4. Simple Social Icons - http://wordpress.org/plugins/simple-social-icons/
+	5. Regenerate Thumbnails - http://wordpress.org/extend/plugins/regenerate-thumbnails/
+
+CREDIT
+	1. Content Boxes by StudioPress - http://www.studiopress.com/design/colored-content-boxes-buttons.htm/
+	2. Gradient Buttons by StudioPress - http://www.studiopress.com/design/colored-content-boxes-buttons.htm/
+	3. Icons for eNews and Search input fields by iconmonstr - http://iconmonstr.com/
+	4. Responsive Menus by Ozzy Rodriguez - http://ozzyrodriguez.com/tutorials/genesis/genesis-responsive-menu-2-0/
+
+FURTHER
+	1. Theme Release Page - http://wpcanada.ca/our-themes/raven/
+	2. Theme Documentation - http://docs.wpcanada.ca/docs/raven/
+	3. Theme Demo - http://demo.wpcanada.ca/raven/
+	4. Download from GitHub - https://github.com/lenkutchma/raven
+
+BUG REPORTS
+	https://github.com/lenkutchma/raven/issues
+
+SUPPORT
+	Support is limited to helping you get your theme up and running just like you see on the demo site. Support does not include customizations.
+
+Enjoy!
---
+++ functions.php
@@ -0,0 +1,137 @@
+<?php
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'raven', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/lib/languages', 'raven' ) );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', __( 'Raven Theme', 'raven' ) );
+define( 'CHILD_THEME_URL', 'http://wpcanada.ca/our-themes/raven/' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5' );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Enqueue Javascript files
+add_action( 'wp_enqueue_scripts', 'raven_enqueue_scripts' );
+function raven_enqueue_scripts() {
+
+	//* Responsive Menu script
+	wp_enqueue_script( 'raven-responsive-menu', get_stylesheet_directory_uri() . '/lib/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+
+	//* Backstretch script
+	if ( ! get_background_image() )
+		return;
+
+	wp_enqueue_script( 'raven-pro-backstretch', get_bloginfo( 'stylesheet_directory' ) . '/lib/js/backstretch.js', array( 'jquery' ), '1.0.0' );
+	wp_enqueue_script( 'raven-pro-backstretch-set', get_bloginfo('stylesheet_directory').'/lib/js/backstretch-set.js' , array( 'jquery', 'raven-pro-backstretch' ), '1.0.0' );
+
+	wp_localize_script( 'raven-pro-backstretch-set', 'BackStretchImg', array( 'src' => str_replace( 'http:', '', get_background_image() ) ) );
+}
+
+//* Enqueue CSS files
+add_action( 'wp_enqueue_scripts', 'raven_enqueue_styles' );
+function raven_enqueue_styles() {
+
+	wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css?family=PT+Serif:400,700|Open+Sans:400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'raven-dashicons-style', get_stylesheet_uri(), array('dashicons'), '1.0' );
+	wp_enqueue_style( 'raven-genericons-style', get_stylesheet_directory_uri() . '/lib/font/genericons.css' );
+}
+
+//* Add new image sizes
+add_image_size( 'mini-square', 80, 80, TRUE );
+add_image_size( 'square', 100, 100, TRUE );
+add_image_size( 'large-square', 120, 120, TRUE );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Reposition the primary navigation
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_before_header', 'genesis_do_nav' );
+
+//* Add the welcome text section to front page
+add_action( 'genesis_before_loop', 'raven_welcome_text' );
+function raven_welcome_text() {
+
+	if ( ! is_front_page() || get_query_var( 'paged' ) >= 2 )
+		return;
+
+	genesis_widget_area( 'welcome_text', array(
+		'before' => '<div class="welcome-text widget-area">',
+		'after'  => '</div>',
+	) );
+
+}
+
+//* Add before entry widget area to single post page
+add_action( 'genesis_before_entry', 'raven_before_entry' ); 
+function raven_before_entry() {
+
+	if ( ! is_singular( 'post' ) )
+		return;
+
+	genesis_widget_area( 'before-entry', array(
+		'before' => '<div class="before-entry widget-area"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+
+}
+
+//* Add support for after entry widget
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+//* Relocate after entry widget
+remove_action( 'genesis_after_entry', 'genesis_after_entry_widget_area' );
+add_action( 'genesis_after_entry', 'genesis_after_entry_widget_area', 5 );
+
+//* Add single post navigation
+add_action( 'genesis_after_entry', 'genesis_prev_next_post_nav', 9 );
+
+//* Customize the post info function
+add_filter( 'genesis_post_info', 'raven_post_info_filter' );
+function raven_post_info_filter( $post_info ) {
+
+	$post_info = '[post_date] [post_author_link] [post_comments] [post_edit]';
+	return $post_info;
+
+}
+
+//* Modify comment form
+add_filter( 'comment_form_defaults', 'raven_comment_form_defaults' );
+function raven_comment_form_defaults( $defaults ) {
+ 
+	$defaults['title_reply'] = __( 'Join the Discussion!' );
+	$defaults['comment_notes_before'] = '<p class="comment-box">' . __( 'Please submit your comment with a real name.' );
+	$defaults['comment_notes_after'] = '<p class="comment-box">' . __( 'Thanks for your feedback!' );
+	return $defaults;
+ 
+}
+
+//* Customize search form placeholder text
+add_filter( 'genesis_search_text', 'raven_search_text' );
+function raven_search_text( $text ) {
+	return esc_attr( 'Search this website...' );
+}
+
+// Add support for structural wraps
+add_theme_support( 'genesis-structural-wraps', array( 'header', 'nav', 'subnav', 'inner', 'footer-widgets', 'footer' ) );
+
+//* Register widget areas
+genesis_register_sidebar( array(
+	'id'          => 'welcome_text',
+	'name'        => __( 'Welcome Text', 'raven' ),
+	'description' => __( 'This is the welcome text widget area.', 'raven' ),
+) );
+genesis_register_sidebar( array(
+	'id'		=> 'before-entry',
+	'name'		=> __( 'Before Entry', 'raven' ),
+	'description'	=> __( 'This is the Before Entry widget area.', 'raven' ),
+) );
